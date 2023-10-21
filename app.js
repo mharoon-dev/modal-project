7e1a5c10111903f3426656c5f5a36ab650b8dbdb
mharoon-dev <haroon.aghani@gmail.com>
2023-10-22 00:28:22 +0500

    complete

---
+++ app.js
@@ -0,0 +1,20 @@
+let card = document.querySelector('.card')
+let button = document.querySelector('button')
+let modalCard = document.querySelector('.modalCard')
+let modalCard_img = document.querySelector('.modalCard-img')
+
+function buttonHandler() {
+    card.style.transform = 'rotate(180deg)';
+    card.style.transition = '2s';
+    modalCard.style.opacity = '1';
+    modalCard.style.pointerEvents = 'auto';
+    modalCard.style.transition = '2s';
+}
+
+function offHandler() {
+    card.style.transform = 'rotate(0deg)';
+    card.style.transition = '2s';
+    modalCard.style.opacity = '0'; 
+    modalCard.style.pointerEvents = 'none';
+    modalCard.style.transition = '2s';
+}
